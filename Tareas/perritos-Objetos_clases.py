e351336d0077c3ebb2b042e6a15e29f9a27dd7b1
adrianpriet <61021661+adrianpriet@users.noreply.github.com>
2021-09-06 23:44:11 -0500

    Create perritos-Objetos_clases.py

---
+++ Tareas/perritos-Objetos_clases.py
@@ -0,0 +1,24 @@
+"""
+# NAME
+        perritos-Objetos_clases.py
+# VERSION
+        1.0
+# AUTHOR
+        Adrian Prieto Castellanos
+# DATE
+        06/09/2021
+# DESCRIPTION
+        este diferencia a distintos tipos de perritos por su tamano, color y trucos que hace al hacer uso de metodos, atributos, clases,
+        subclases, herencia, overriding y polimorfismos.
+# ARGUMENTS
+        No hay argumentos
+# EXAMPLES
+        pug = dificil ('cafe', 'chico', 'Se hace el muertito')
+        print(pug.__dict__)
+        pug.hace_trucos()
+   OUTPUT
+        {'color': 'cafe', 'tamano': 'chico', 'trucos': 'Se hace el muertito'}
+        Se hace el muertito
+# GITHUB LINK
+        
+"""
