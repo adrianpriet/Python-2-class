61a814b2ebf7362346d13120493480089c08dda1
adrianpriet <61021661+adrianpriet@users.noreply.github.com>
2021-10-11 09:44:34 -0500

    Update Tarea_Entrez.py

--- Tareas/Tarea_Entrez.py
+++ Tareas/Tarea_Entrez.py
@@ -3,18 +3,20 @@
   Tarea_Entrez.py
   
 # VERSION
-  1.0
+  2.0
   
 # AUTOR
   Adrian Prieto Castellanos
   
 # FECHA
-  4 octubre 2021
+  11 octubre 2021
   
 # DESCRIPCION
   Este programa primero nos muestra la descripción de los siguientes campos de la base de datos "protein"
   - FieldList "ECNO"
   - LinkList "protein_protein_small_genome"
+  Para despues guardar los ID´s correspondiantes a terminos de busqueda en las bases de datos,
+  posteriormente obtiene los abtracts de esos ID´s y tambien guarda los ID´s de las citas del articulo
 
 # USO
     
@@ -24,6 +26,7 @@
     Terminos a buscar
 # OUTPUT
     Documento donde con los ID´s correspondientes al los terminos de busqueda 
+    Documento con los abstracts y los ID´s de las citas 
 # REQUERIMIENTOS:
     python3
 # FUENTE
@@ -62,3 +65,24 @@ archivo = open("ID´s.txt",'w')
         archivo.write(elemento)
     
 archivo.close()
+
+#Creamos un archivo en donde guardaremos los abstracts y los ID´s de las citas
+abstracts = open("Abstracts.txt",'w')
+for elemento in id_list:
+  #Buscamos y guardamos los abstracts con efetch
+    fetch_handle = Entrez.efetch(db="pubmed", id=elemento,
+                                rettype="abstract", retmode="text")
+    data = fetch_handle.read()
+    abstracts.write(data) 
+    fetch_handle.close() #Cerramos el handle 
+    
+    #Buscamos los ID´s del articulo
+    results = Entrez.read(Entrez.elink(dbfrom="pubmed", db="pmc",
+                                  LinkName="pubmed_pmc_refs", from_uid=elemento))
+    #Guardamos los ID´s de las citas en una lista 
+    pmc_ids = [link["Id"] for link in results[0]["LinkSetDb"][0]["Link"]]
+    #Guardamos los ID´s de las citas en el documento debajo del abstract correspondiente
+    for cita in pmc_ids 
+        abstracts.write(cita)
+#Cerramos el archivo 
+abstracts.close()
